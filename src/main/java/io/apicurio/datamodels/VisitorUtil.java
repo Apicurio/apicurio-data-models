9db0d028fb118e3d5f265f6072ff330f73b795dc
Eric Wittmann <eric.wittmann@gmail.com>
2022-11-29 19:01:29 -0500

    Some basic Library functionality to get going.

---
+++ src/main/java/io/apicurio/datamodels/VisitorUtil.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2019 Red Hat
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.apicurio.datamodels;
+
+import io.apicurio.datamodels.asyncapi.v20.visitors.AsyncApi20Traverser;
+import io.apicurio.datamodels.asyncapi.v21.visitors.AsyncApi21Traverser;
+import io.apicurio.datamodels.asyncapi.v22.visitors.AsyncApi22Traverser;
+import io.apicurio.datamodels.asyncapi.v23.visitors.AsyncApi23Traverser;
+import io.apicurio.datamodels.asyncapi.v24.visitors.AsyncApi24Traverser;
+import io.apicurio.datamodels.asyncapi.v25.visitors.AsyncApi25Traverser;
+import io.apicurio.datamodels.openapi.v20.visitors.OpenApi20Traverser;
+import io.apicurio.datamodels.openapi.v30.visitors.OpenApi30Traverser;
+import io.apicurio.datamodels.openapi.v31.visitors.OpenApi31Traverser;
+import io.apicurio.datamodels.visitors.ReverseTraverser;
+import io.apicurio.datamodels.visitors.Traverser;
+import io.apicurio.datamodels.visitors.Visitor;
+
+/**
+ * @author eric.wittmann@gmail.com
+ */
+public class VisitorUtil {
+
+    public static void visitTree(DocumentType type, Node node, Visitor visitor, TraverserDirection direction) {
+        Traverser traverser;
+        if (direction == TraverserDirection.up) {
+            traverser = new ReverseTraverser(visitor);
+        } else {
+            switch (type) {
+                case ASYNCAPI_20:
+                    traverser = new AsyncApi20Traverser(visitor);
+                case ASYNCAPI_21:
+                    traverser = new AsyncApi21Traverser(visitor);
+                case ASYNCAPI_22:
+                    traverser = new AsyncApi22Traverser(visitor);
+                case ASYNCAPI_23:
+                    traverser = new AsyncApi23Traverser(visitor);
+                case ASYNCAPI_24:
+                    traverser = new AsyncApi24Traverser(visitor);
+                case ASYNCAPI_25:
+                    traverser = new AsyncApi25Traverser(visitor);
+                case OPENAPI_2:
+                    traverser = new OpenApi20Traverser(visitor);
+                case OPENAPI_30:
+                    traverser = new OpenApi30Traverser(visitor);
+                case OPENAPI_31:
+                    traverser = new OpenApi31Traverser(visitor);
+                default:
+                    throw new RuntimeException("Traverser not found for type: " + type);
+            }
+        }
+        traverser.traverse(node);
+    }
+
+}
