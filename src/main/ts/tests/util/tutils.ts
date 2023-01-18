47b2ee16b85ab93bc5ede24ba6c4ca7fd874258d
Eric Wittmann <eric.wittmann@gmail.com>
2023-01-18 13:49:09 -0500

    Validation tests in the TS layer

--- src/main/ts/tests/util/tutils.ts
+++ src/main/ts/tests/util/tutils.ts
@@ -1,5 +1,7 @@
+import {ValidationProblemSeverity} from "../../src/io/apicurio/datamodels/validation/ValidationProblemSeverity";
+import {ValidationProblem} from "../../src/io/apicurio/datamodels/validation/ValidationProblem";
 
-var fs = require("fs");
+const fs = require("fs");
 
 export function readJSON(path: string): any {
     let fileContent: string = fs.readFileSync(path);
@@ -15,42 +17,38 @@ export function readTXT(path: string): string {
     return "";
 }
 
-//
-// export function formatSeverity(severity: ValidationProblemSeverity): string {
-//     if (severity == ValidationProblemSeverity.ignore) {
-//         return "ignore";
-//     } else if (severity == ValidationProblemSeverity.low) {
-//         return "low";
-//     } else if (severity == ValidationProblemSeverity.medium) {
-//         return "medium";
-//     } else if (severity == ValidationProblemSeverity.high) {
-//         return "high";
-//     }
-// }
-//
-//
-// export function readSeverity(severity: string): ValidationProblemSeverity {
-//     if (severity == "ignore") {
-//         return ValidationProblemSeverity.ignore;
-//     } else if (severity == "low") {
-//         return ValidationProblemSeverity.low;
-//     } else if (severity == "medium") {
-//         return ValidationProblemSeverity.medium;
-//     } else if (severity == "high") {
-//         return ValidationProblemSeverity.high;
-//     }
-// }
 
-//
-// export function formatProblems(problems: ValidationProblem[]): string[] {
-//     let es: string[] = [];
-//     problems.forEach(problem => {
-//         es.push(`[${problem.errorCode}] |${formatSeverity(problem.severity)}| {${problem.nodePath.toString()}->${problem.property}} :: ${problem.message}`);
-//     });
-//     return es;
-// }
-
-
-export function normalize(value: string): string {
-    return value.trim().replace("\r\n", "\n");
+export function formatSeverity(severity: ValidationProblemSeverity): string {
+    if (severity == ValidationProblemSeverity.ignore) {
+        return "ignore";
+    } else if (severity == ValidationProblemSeverity.low) {
+        return "low";
+    } else if (severity == ValidationProblemSeverity.medium) {
+        return "medium";
+    } else if (severity == ValidationProblemSeverity.high) {
+        return "high";
+    }
 }
+
+
+export function readSeverity(severity: string): ValidationProblemSeverity {
+    if (severity == "ignore") {
+        return ValidationProblemSeverity.ignore;
+    } else if (severity == "low") {
+        return ValidationProblemSeverity.low;
+    } else if (severity == "medium") {
+        return ValidationProblemSeverity.medium;
+    } else if (severity == "high") {
+        return ValidationProblemSeverity.high;
+    }
+}
+
+
+export function formatProblems(problems: ValidationProblem[]): string[] {
+    let es: string[] = [];
+    problems.forEach(problem => {
+        es.push(`[${problem.errorCode}] |${formatSeverity(problem.severity)}| {${problem.nodePath.toString(true)}->${problem.property}} :: ${problem.message}`);
+    });
+    return es;
+}
+
